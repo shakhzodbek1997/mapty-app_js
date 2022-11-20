99efc4c9043fe1a012c819843a75ccc1195a1517
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-11-20 23:24:46 +0100

    Workout marker

--- script.js
+++ script.js
@@ -22,18 +22,33 @@ if (navigator.geolocation) {
     
       const coords = [latitude, longitude]
 
-      // ---------------------Leaflet library added-------------------- --
+      // ---------------------Leaflet library added---------------------
       const map = L.map('map').setView(coords, 13);
-
+    //   console.log(map);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      
+      map.on('click', function(mapEvent){
+        console.log(mapEvent);
+        const{lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                })
+            )
+            .setPopupContent("Workout")
+            .openPopup();
+      })
     },
     // error call back function
     function () {
