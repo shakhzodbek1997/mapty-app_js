305e41c9975b915e474d926d9d5f1961c24d8d68
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-11-20 22:09:26 +0100

    Map added

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  // Geolocation is aan API
+  navigator.geolocation.getCurrentPosition(
+    // success call back function
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    
+      const coords = [latitude, longitude]
+
+      // ---------------------Leaflet library added-------------------- --
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    // error call back function
+    function () {
+      alert('Could not GET your Position!');
+    }
+  );
+};
